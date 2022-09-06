9cc600f376e9627f6826a43446c2ff5f6f28225b
Кирилл Чемизов <kirill20.04.2002@yandex.ru>
2022-09-06 19:38:05 +0300

    добавил модельки categore, title,genre

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from .validators import year_validator
+class Category(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Категория')
+    slug = models.SlugField(unique=True, verbose_name='Слаг категорий')
 
-# Create your models here.
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+class Genre(models.Model):
+    genre = models.CharField(verbose_name='Жанр')
+    slug = models.SlugField(unique=True, verbose_name='Слаг жанра')
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанры"
+
+    def __str__(self):
+        return self.name
+
+class Title(models.Model):
+    category = models.ForeignKey(Category, 
+                                 verbose_name='Категория произведения')
+    genre = models.ManyToManyField(Genre, 
+                                   verbose_name='Жанр произведения')
+    name = models.CharField(verbose_name='Название произведения')
+    year = models.IntegerField(validators=year_validator,
+                               verbose_name='Год выпуска произведения')
+    description = models.CharField(verbose_name='Описание')
+
+    class Meta:
+        verbose_name = "Произведение"
+        verbose_name_plural = "Произведения"
+
+    def __str__(self):
+        return self.name
---
+++ api_yamdb/reviews/validators.py
@@ -0,0 +1,8 @@
+
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+
+def year_validator(value):
+    if not (0 < value <= timezone.now().year):
+        raise ValidationError('Проверьте год произведения.')
