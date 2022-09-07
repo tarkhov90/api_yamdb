16223dbb5705de0221dce2d7258d5425169289a6
Александр Фомичев <fan160688@yandex.ru>
2022-09-07 21:11:56 +0300

    Перенес модели в reviews, создал файл data_load для скрипта загрузки данных из csv

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -3,16 +3,21 @@ from .models import Review, Comment, Title
 from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 from .serializers import (CommentSerializer, ReviewSerializer)
+from django.db.models import Avg
 from rest_framework.permissions import IsAuthenticated
 
 from .permissions import IsOwnerOrReadOnly
 # Create your views here.
 
 
+class TitleViewSet(viewsets.ModelViewSet):
+    avg_score = Review.objects.aggregate(Avg('score'))
+
+
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
     permission_classes = [IsOwnerOrReadOnly]
-   
+
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
         return title.reviews.all()
@@ -25,7 +30,7 @@ class ReviewViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = [IsOwnerOrReadOnly]
-   
+
     def get_queryset(self):
         review = get_object_or_404(Comment, pk=self.kwargs.get('review_id'))
         return review.comments.all()
