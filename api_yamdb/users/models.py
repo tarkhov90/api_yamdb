5d51fdd1b54d3b04b76c0266cf8fe83be988a995
tarkhov90 <99458741+tarkhov90@users.noreply.github.com>
2022-09-12 00:00:57 +0500

    after 1st review

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -47,7 +47,7 @@ class User(AbstractUser):
     role = models.CharField(
         max_length=20,
         choices=ROLES,
-        default='user',
+        default=USER,
     )
     bio = models.TextField(
         max_length=500,
